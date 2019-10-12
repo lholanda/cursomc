1ade742c4711876e00b225d730bb164aa1d446fc
Luiz Holanda <luiz.holanda1408@gmail.com>
2019-10-11 21:33:50 -0300

    Estado e Cidade

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Estado;
 import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.CidadeRepository;
+import com.nelioalves.cursomc.repositories.EstadoRepository;
 import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 // implements ComandLineRunner, faz com que seja rodado automaticamente
@@ -20,9 +24,13 @@ public class CursomcApplication implements CommandLineRunner{
 	// CursoApplication
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -30,25 +38,40 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Metodo auxIliar quando a aplicacao iniciar
+		// Metodo auxiliar quando a aplicacao iniciar
+
+		// CATEGORIAS E PRODUTOS
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");	
 		
 		Produto   p1   = new Produto(null, "Computador", 2000.00);
 		Produto   p2   = new Produto(null, "Impressora", 800.00);
 		Produto   p3   = new Produto(null, "Mouse", 80.00);
-	
+		// Categorias Associacoes
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+		// Produtos Associacoes
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		// salva categorias
+		// salvar
 		categoriaRepository.save(Arrays.asList(cat1, cat2)); 
-		// salva os produtos
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		
+		// ESTADOS E CIDADES
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		// Cidades tem que conhecer os estados
+		Cidade c1 = new Cidade(null, "Uberlândia",est1);
+		Cidade c2 = new Cidade(null, "São Paulo" ,est2);
+		Cidade c3 = new Cidade(null, "Campinas"  ,est2);
+		// Estados devem conhecer as cidades
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		// salvar
+		estadoRepository.save(Arrays.asList(est1, est2)); 
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+	
 	}
 
 }
